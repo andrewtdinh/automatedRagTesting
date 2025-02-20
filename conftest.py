6d1be785138d563287fa044a11ada746f18a391c
Andrew Dinh <andrewdinh74@yahoo.com>
2025-02-19 22:02:14 -0800

    Introduce conftest to create a global fixture for all tests to share

--- conftest.py
+++ conftest.py
@@ -1,26 +1,6 @@
 import pytest
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from ragas import SingleTurnSample
-from ragas.llms import LangchainLLMWrapper
-from ragas.metrics import LLMContextRecall
 import requests
-
-load_dotenv()
-
-@pytest.mark.asyncio
-async def test_context_recall(llm_wrapper, get_data):
-    context_recall =  LLMContextRecall(llm=llm_wrapper)
-    score = await context_recall.single_turn_ascore(get_data)
-    print(f"Context Recall Score: {score}")
-    assert score > 0.7
-
-@pytest.fixture
-def llm_wrapper():
-    llm = ChatOpenAI(model="gpt-4", temperature=0)
-    langchain_llm = LangchainLLMWrapper(llm)
-
-    return langchain_llm
+from ragas import SingleTurnSample
 
 @pytest.fixture
 def get_data():
--- test_context_recall.py
+++ test_context_recall.py
@@ -1,31 +1,22 @@
 import pytest
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from ragas import SingleTurnSample
 from ragas.llms import LangchainLLMWrapper
 from ragas.metrics import LLMContextRecall
-import requests
 
 load_dotenv()
 
 @pytest.mark.asyncio
-async def test_context_recall():
+async def test_context_recall(llm_wrapper, get_data):
+    context_recall =  LLMContextRecall(llm=llm_wrapper)
+    score = await context_recall.single_turn_ascore(get_data)
+    print(f"\nContext Recall Score: {score}")
+    assert score > 0.7
+
+@pytest.fixture
+def llm_wrapper():
     llm = ChatOpenAI(model="gpt-4", temperature=0)
     langchain_llm = LangchainLLMWrapper(llm)
-    context_recall =  LLMContextRecall(llm=langchain_llm)
-    question = "How many articles are there in the Selenium webdriver python course?"
-    response_dict = requests.post('https://rahulshettyacademy.com/rag-llm/ask',
-                                  json={
-                                      "question": question,
-                                      "chat_history": []
-                                  }).json()
 
+    return langchain_llm
 
-    sample = SingleTurnSample(
-        user_input=question,
-        retrieved_contexts=[retrieved_context['page_content'] for retrieved_context in response_dict['retrieved_docs']],
-        reference="23"
-    )
-    score = await context_recall.single_turn_ascore(sample)
-    print(f"Context Recall Score: {score}")
-    assert score > 0.7
